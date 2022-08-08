488d6226473c101b4341910b14628655be69586a
SneriKob <sneri.github@proton.me>
2022-08-08 10:31:32 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import ListContainer from "./ListContainer.js"
 export default function App() {
   const Title = () => {
     return (
-      <div className='row'>
-        <div className='col'>
-         <h1>
-          TO DOs
-          </h1>
-        </div>
-      </div>
+      <nav class="navbar navbar-light bg-dark">
+        <span class="navbar-brand h1 text-light">Interaktive To Do Liste</span>
+      </nav>
     )
   } 
 
